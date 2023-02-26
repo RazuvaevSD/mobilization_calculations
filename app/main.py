645077310da9a956ee9ae5c28f050ee2e4b2a5dc
Сергей Разуваев <razuvaev.sd@ya.ru>
2023-02-27 01:23:15 +1100

    init app

--- app/main.py
+++ app/main.py
@@ -16,23 +16,27 @@ app.geometry(WindowConf.SIZE.value)
 main_menu = MainMenu()
 main_menu.add_command(label='Открыть список документов')
 app.config(menu=main_menu)
-# привяжем функцию открытия таба для списка документов
-# т.к. она заранее предопределена
-# self.main_menu.entryconfigure(0, command=self.open_doc_list_tab)
 
 tab_control = TabControl(master=app.main_frame)
-tab = tab_control.add_tab(text='asdfasdf \n')
 
 
 def foo(a):
     print('print test text', a)
 
 
-menu = TabMenu(master=tab, bg='white')
-menu.add_btn(text='btn1', command=lambda: foo(1))
-menu.add_btn(text='btn2', command=lambda: foo(2))
-lbl_test = Label(master=tab, text='test_text', bg='white')
-lbl_test.pack()
+def open_tab():
+    tab = tab_control.add_tab(text='asdfasdf \n')
+    menu = TabMenu(master=tab, bg='white')
+    menu.add_btn(text='btn1', command=lambda: foo(1))
+    menu.add_btn(text='btn2', command=lambda: foo(2))
+    lbl_test = Label(master=tab, text='test_text', bg='white')
+    lbl_test.pack()
+
+
+# привяжем функцию открытия таба для списка документов
+# т.к. она заранее предопределена
+main_menu.entryconfigure(0, command=open_tab)
+
 
 
 app.mainloop()
