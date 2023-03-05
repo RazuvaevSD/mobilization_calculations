7f906f78868302533c04fcbdd1e4bea2931858bc
Сергей Разуваев <razuvaev.sd@ya.ru>
2023-03-06 08:43:58 +1100

    update config: add db

--- app/core/config.py
+++ app/core/config.py
@@ -1,4 +1,13 @@
+import os
 from enum import Enum
+
+from dotenv import load_dotenv
+
+dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
+if os.path.exists(dotenv_path):
+    load_dotenv(dotenv_path)
+
+DB_URI = os.getenv('DB_URI', 'sqlite:///story.db')
 
 
 class WindowConf(str, Enum):
--- app/core/db.py
+++ app/core/db.py
@@ -2,7 +2,7 @@ from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import Session
 
-from .settings import DB_URI
+from app.core.config import DB_URI
 
 Base = declarative_base()
 
